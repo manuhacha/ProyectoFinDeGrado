bd51862eb5ce48b796e0e3808bf481105bf29ce0
Manuel Ibañez Hacha <manuel.ibanez.external@atos.net>
2024-04-12 12:57:13 +0200

    Homepage y configuracion de rutas

---
+++ front/src/app/bands/bands.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bands',
+  standalone: true,
+  imports: [],
+  templateUrl: './bands.component.html',
+  styleUrl: './bands.component.css'
+})
+export class BandsComponent {
+
+}
