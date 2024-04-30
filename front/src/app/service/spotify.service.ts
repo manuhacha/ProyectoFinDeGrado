c3e6189e609a2d3ce6ff4b12e280347c7a567e64
Manuel Ibañez Hacha <manuel.ibanez.external@atos.net>
2024-04-30 13:50:12 +0200

    Creamos el servicio que gestionará Spotify

--- front/src/app/service/spotify.service.ts
+++ front/src/app/service/spotify.service.ts
@@ -1,21 +1,58 @@
+// spotify.service.ts
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-@Injectable({
-  providedIn: 'root'
-})
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/share';
+
+@Injectable()
 export class SpotifyService {
 
-  private apiArtistsUrl = 'https://api.spotify.com/v1/artists/'
+  private clientId = 'YOUR_CLIENT_ID';
+  private clientSecret = 'YOUR_CLIENT_SECRET';
+  private redirectUri = 'http://localhost:4200/callback';
+  private spotifyApiUrl = 'https://api.spotify.com/v1';
 
-  private accessToken = '7180600d4ab9402aa13798a1e2f7b543'
+  private accessToken: string;
+  private accessTokenSubject: any;
 
   constructor(private http: HttpClient) { }
 
-  getArtist(id:string):Observable<any> {
-    const headers = new HttpHeaders({
-      Authorization: `Bearer ${this.accessToken}`
-    });
-    return this.http.get<any>(this.apiArtistsUrl + id, {headers})
+  getAccessToken(): Observable<string> {
+    if (!this.accessTokenSubject) {
+      this.accessTokenSubject = this.fetchAccessToken()
+        .share();
+    }
+
+    return this.accessTokenSubject;
   }
+
+  private fetchAccessToken(): Observable<string> {
+    if (this.accessToken) {
+      return Observable.of(this.accessToken);
+    }
+
+    const authUrl = `https://accounts.spotify.com/authorize?client_id=${this.clientId}&response_type=code&redirect_uri=${this.redirectUri}&scope=user-read-artist`;
+    window.location.href = authUrl;
+
+    return Observable.fromEvent(window, 'message')
+      .filter((event: MessageEvent) => event.source === window && event.data.type === 'spotify-auth')
+      .map((event: MessageEvent) => event.data.authorizationCode)
+      .switchMap(code => {
+        const body = `grant_type=authorization_code&code=${code}&redirect_uri=${this.redirectUri}&client_id=${this.clientId}&client_secret=${this.clientSecret}`;
+        return this.http.post('https://accounts.spotify.com/api/token', body, { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } })
+          .map(response => response.json())
+          .map(data => {
+            this.accessToken = data.access_token;
+            return data.access_token;
+          });
+      })
+      .catch(error => {
+        console.error('Error getting access token:', error);
+        return Observable.throw(error);
+      });
+  }
+
+  // ... other methods for interacting with the Spotify API
 }
