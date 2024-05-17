b8e4b2ef767f2fc3bab40cb51b65868e11d95cb2
Manuel Ibañez Hacha <manuel.ibanez.external@atos.net>
2024-05-17 17:37:24 +0200

    Quito logout para subirlo a su rama

--- front/src/app/service/auth.service.ts
+++ front/src/app/service/auth.service.ts
@@ -32,12 +32,6 @@ export class AuthService {
   isLogged() {
     return !!localStorage.getItem('token')
   }
-  logout() {
-    localStorage.removeItem('token')
-    localStorage.removeItem('email')
-    this.cookie.delete('spotifytoken')
-    this.router.navigate(['/login'])
-  }
   //Obtenemos la informacion del usuario descifrando el token, que está siendo enviado en la constante headers
   getUserbyToken(token:string) {
     const headers = new HttpHeaders({
