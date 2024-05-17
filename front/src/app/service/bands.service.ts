10dbe948b9af566c8b4ee9a6d9c6675a51d4247e
Manuel Ibañez Hacha <manuel.ibanez.external@atos.net>
2024-05-17 16:57:14 +0200

    Inicio el front y el back y subo la página Home

---
+++ front/src/app/service/bands.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BandsService {
+
+  private getBandsUrl = "http://localhost:3000/api/v1/artist"
+
+
+  constructor(private http: HttpClient) { }
+
+  getArtists() {
+    return this.http.get<any>(this.getBandsUrl)
+  }
+  createArtist(band:any) {
+    return this.http.post<any>(this.getBandsUrl,band)
+  }
+}
